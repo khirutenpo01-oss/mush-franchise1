55d0a759ad4bb785c366d50fe929e4ed0cf6c972
Tenpo Khiru <khirutenpo01@gmail.com>
2026-09-05 22:49:33 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1213,5 +1213,14 @@ function formatDate(date) {
 /* ==========================================================
    START
 ========================================================== */
+window.addEventListener(
+  "mush:navigate",
+  event => {
+    const page = event.detail?.page;
 
+    if (page) {
+      navigate(page);
+    }
+  }
+);
 init();
